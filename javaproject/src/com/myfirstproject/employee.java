b9fb27633751e1979fd79bdc870610f79f28a0d5
kradh <kradh@hp>
2026-08-12 12:59:07 +0530

    ra

---
+++ javaproject/src/com/myfirstproject/employee.java
@@ -0,0 +1,17 @@
+package com.myfirstproject;
+
+public class employee {
+	public static void main(String[]args) {
+	String name="radhika";
+	int age=22;
+	int salary=45000;
+	System.out.println(name);
+	System.out.println(age);
+	System.out.println(salary);
+	}
+}
+
+
+	
+
+
