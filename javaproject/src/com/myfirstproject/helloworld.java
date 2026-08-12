dd57d364ef242c8dbeed144d913e1abca53214f3
kradh <kradh@hp>
2026-08-12 12:06:24 +0530

    ra

--- javaproject/src/com/myfirstproject/helloworld.java
+++ javaproject/src/com/myfirstproject/helloworld.java
@@ -2,7 +2,7 @@ package com.myfirstproject;
 
 public class helloworld {
 	public static void main (String[]args) {
-		System.out.println("hello radhika");
+		System.out.println("java is simple");
 	}
 
 }
